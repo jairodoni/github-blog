3ab7edac2ede9b67e642d3f1db5711df210e7c22
Jairo Doni <jairo.doni97@gmail.com>
2023-09-23 20:42:06 -0300

    ajuste para deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
       </Head>
       <body>
         <Main />
