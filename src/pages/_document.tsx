a177303f6e1da2db4c2cc6d70928b7cf73b2baa3
Jairo Doni <jairo.doni97@gmail.com>
2023-09-24 15:55:56 -0300

    favicon trocado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           httpEquiv="Content-Security-Policy"
